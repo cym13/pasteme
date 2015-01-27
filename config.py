3d564cacf42c8904be31eb398e14a31cf387ed30
VG <vg@devys.org>
2015-01-27 12:15:04 +0100

    Fix configuration file to change directory automatically
    - the pastes store directory reflects environment: /tmp/pastes for developing
      and /pastes for production.

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # configuration file of pastme service
 
+import os.path
+
 # the directory where the pastes are stored
-pastedir = '/tmp/pastes'
+pastedir = '/pastes' if os.path.exists('/pastes') else '/tmp/pastes'
 
 # how many days a paste remain stored
 timeout = 30
